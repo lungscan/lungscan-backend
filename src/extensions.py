336465e76b8af7f3c686e356540cdaac8f6ffa94
lucasvieiras <lucasvieirasilva.lpta@gmail.com>
2025-06-12 23:49:09 -0300

    chore: remove default rate limits

--- src/extensions.py
+++ src/extensions.py
@@ -3,6 +3,5 @@ from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "500 per hour"],
     storage_uri="memory://",
 ) 
